1cb1e5170b9a5435e57aca09a4e3aa7086feef12
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-11 15:10:39 -0500

    A type-annotated function - takes a float as argument and returns the floor of the float.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+type-annotated function that takes a float as argument
+returns the floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    returns floor of the float
+    """
+    return int(n // 1)
