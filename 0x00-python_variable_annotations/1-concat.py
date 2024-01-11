400b321720950aa6782edb4a7ce292a6389252f3
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-11 10:31:14 -0500

    A type-annotated function concat that takes 2 strings str1 and str2 as arguments and returns a concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function that takes 2 string
+(str1 and str2) as arguments and returns a
+concatenated string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    returns a concatenated string of str1 and str2
+    """
+    return str1 + str2
