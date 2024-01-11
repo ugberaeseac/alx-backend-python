c10e8edb800fd053dddbad603f015840a17c8e0b
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-11 10:16:38 -0500

    A type-annotated function that returns the sum of two floats as a float

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function that takes 2 floats as arguments
+and returns their sum as a float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    sum up 2 floats
+    """
+    return (a + b)
