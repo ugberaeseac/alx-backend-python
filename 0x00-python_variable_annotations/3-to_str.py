db06b133be56513838276bd640fd8e3b1c76777a
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-11 15:17:44 -0500

    A type-annotated function - takes a float as argument and returns the string representation of the float.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+type-annotated function that takes a float as argument
+returns the floor of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    returns floor of the float
+    """
+    return str(n)
