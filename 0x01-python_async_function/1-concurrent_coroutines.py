8b567947f73c8d0b3105437d6028cb719ef1d7af
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-15 18:14:06 -0500

    Asynchronous coroutine that spawans multiple coroutines at the same time

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Asynchronous coroutine that spawans
+multiple coroutines at the same time
+Returns a list of all the delays in ascending order
+"""
+
+
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """
+    execute wait_random - n times
+    return the list of delays in
+    ascending order using sorted()
+    """
+    async_tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*async_tasks)
+    return sorted(delays)
