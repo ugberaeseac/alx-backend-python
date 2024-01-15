65a828d0965da633119872e8dfef62c1c357739c
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-15 18:22:14 -0500

    Asynchronous coroutine that spawns multiple coroutines at the same time

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,10 +7,11 @@ Returns a list of all the delays in ascending order
 
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     spawns wait_random n times
     return the list of delays in
