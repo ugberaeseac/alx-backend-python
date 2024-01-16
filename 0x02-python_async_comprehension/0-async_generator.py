a580c26785faafbbc5a419c036f2a49f942eb294
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-16 08:37:32 -0500

    Async generator - a coroutine that loops 10 times, async waits a second and yields a random number

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Async generator - coroutine loops 10times
+asynchronously wait a second then yield a random
+number between 0 and 10.
+"""
+
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    loop, asynchronously wait 1 sec and
+    yield a random number
+    """
+    for _ in range(10):
+        number = random.uniform(0, 10)
+        await asyncio.sleep(1)
+        yield number
