66d048db2fb54ff1084b50558bfea517f6a24f6b
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-16 17:35:03 -0500

    measure the run time

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+
+"""
+
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+
+    """
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.perf_counter() - start_time
+    return end_time / n
