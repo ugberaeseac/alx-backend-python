11ee1271f2bbb5c8243a1b59e8428243978ea763
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-16 09:03:07 -0500

    async comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+async comprehension
+import async_generator from 0-async_generator
+returns list of 10 random numbers using
+async comprehension
+"""
+
+
+from typing import Generator
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Generator[float, None, None]:
+    """
+    return 10 random numbers using async comprehension
+    """
+    random_numbers = [number async for number in async_generator()]
+    return random_numbers
