0d54153ad1b5745f15ba9251f97708a68f340b2e
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-19 17:57:23 -0500

    async tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """
-
+async coroutine that spawns
+a function multiple times at the same time
+Returns a list of all the delays in ascending order
 """
 
 
@@ -11,7 +13,9 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-
+    spawns task_wait_random n times
+    return the list of delays in
+    ascending order
     """
     async_tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*async_tasks)
