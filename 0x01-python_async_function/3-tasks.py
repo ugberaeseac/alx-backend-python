122450ef7ce386729609246b61548bf022bbf98a
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-19 17:33:33 -0500

    A function that returns an asyncio task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-
+A function that returns an asyncio task
 """
 
 
@@ -10,7 +10,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 def task_wait_random(max_delay: int = 10) -> asyncio.Task:
     """
-
+    returns asyncio task
     """
     task = asyncio.create_task(wait_random(max_delay))
     return task
