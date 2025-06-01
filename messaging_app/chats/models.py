c4ae01c93ee72df9baee78ae90740a5421614cfd
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2025-06-01 11:28:02 -0400

    fix: fixed schema and added new required fields

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,19 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.conf import settings
+import uuid
 
 # Create your models here.
 
 class User(AbstractUser):
     """ """
-    bio = models.TextField(blank=True, null=True)
-
+    user_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    
     def __str__(self):
         return 'User ({}, {}, {})'.format(self.id, self.username, self.email)
 
 
 class Conversation(models.Model):
     """ """
+    conversatation_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
     participants = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='conversations')
     created_on = models.DateTimeField(auto_now_add=True)
 
@@ -23,10 +26,12 @@ class Conversation(models.Model):
 
 class Message(models.Model):
     """ """
+    message_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
     sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_messages')
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return 'Message ({} - {})'.format(self.sender.username, self.content[:30])
