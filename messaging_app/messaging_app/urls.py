e86c449686704c96817a71a583a8ecd5fb21fee7
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2025-06-06 14:48:59 -0400

    fix: fixed rest_framework api-auth

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('api/', include('chats.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
