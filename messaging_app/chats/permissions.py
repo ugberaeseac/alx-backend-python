697d01f90d1b2587d5c224adf93f7638a129eedd
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2025-06-08 13:39:55 -0400

    fix: Ensured only authenticated users can access API data and specific objects they have permissions to interact with

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,6 +1,21 @@
 from rest_framework import permissions
 
 class IsParticipantOfConversation(permissions.BasePermission):
+    """
+    allow participants of a conversation to read, write and delete conversation(s)
+    """
+    def has_permissions(self, request, view):
+        """
+        checks if user is aunthenticated
+        """
+        return request.user and request.user.is_authenticated
+
+
     def has_object_permission(self, request, view, obj):
-        return request.user in obj.participants.object.all()
+        """
+        checks if user has permission to interact with Conversation Object
+        """
+        if request.method in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
+            return request.user in obj.participants.object.all()
+        return False
 
