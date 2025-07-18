298fc4414d7642ca8671cdc9f39f333f4f1a6d13
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2025-07-18 18:02:39 -0400

    structure file for checker

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+from django.http import HttpResponseForbidden
 
 
 class RequestLoggingMiddleware():
@@ -17,4 +18,27 @@ class RequestLoggingMiddleware():
 
         response = self.get_response(request)
         return response
+
+
+class RestrictAccessByTimeMiddleware():
+    """
+    middlewate that restricts access to the messaging app
+    during certain hours of the day
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_hour = datetime.now().hour
+
+        if (18 <= current_hour <= 21):
+            return HttpResponseForbidden('Access is forbidden outside 6PM and 9PM')
+        response = self.get_response(request)
+        return response
+
+
+
+
+
+
 
